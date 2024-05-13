f0e8b629a422d50e378af3340e9758153468d54a
Hanyel Chamon <hanyel.chamon@gmail.com>
2024-05-13 16:11:37 -0300

    add typescript

--- after/plugin/ale.lua
+++ after/plugin/ale.lua
@@ -2,5 +2,6 @@ vim.g.ale_fix_on_save = 1
 
 vim.g.ale_fixers = {
     ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
-    ['javascript'] = {'prettier', 'eslint'}
+    ['javascript'] = {'prettier', 'eslint'},
+    ['typescript'] = {'prettier', 'eslint'}
 }
